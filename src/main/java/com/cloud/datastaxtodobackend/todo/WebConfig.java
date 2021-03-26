4a85fe9574398c0cb55de6280f452b68649d2c40
Vũ Ngọc Linh <nlinhvu.cs@gmail.com>
2021-03-26 12:11:56 +0700

    fix: update WebConfig

--- src/main/java/com/cloud/datastaxtodobackend/todo/WebConfig.java
+++ src/main/java/com/cloud/datastaxtodobackend/todo/WebConfig.java
@@ -9,6 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*");
     }
 }
